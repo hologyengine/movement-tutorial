a550f3545275b68166c13f1080f10a3fad379f0a
Adam Ringhede <adamringhede@live.com>
2024-05-26 12:09:54 +0200

    initial commit

---
+++ src/actors/character.ts
@@ -0,0 +1,70 @@
+
+import { Actor, AnimationState, AnimationStateMachine, AssetLoader, BaseActor, ViewController, attach, inject } from "@hology/core/gameplay";
+import { CharacterAnimationComponent, CharacterMovementComponent, CharacterMovementMode, ThirdPartyCameraComponent } from "@hology/core/gameplay/actors";
+
+@Actor()
+class Character extends BaseActor {
+
+  private viewController = inject(ViewController)
+  private animation = attach(CharacterAnimationComponent)
+  public readonly movement = attach(CharacterMovementComponent, {
+    maxSpeed: 1.5,
+    maxSpeedSprint: 4,
+    maxSpeedBackwards: 1,
+    snapToGround: 0.1,
+    autoStepMinWidth: 0,
+    autoStepMaxHeight: 0.1,
+    fallingReorientation: true,
+    fallingMovementControl: 0.2,
+    colliderHeight: .4,
+    colliderRadius: 0.2,
+    jumpVelocity: 3.5
+  })
+  public readonly thirdPartyCamera = attach(ThirdPartyCameraComponent, {
+    height: .7,
+    offsetX: -0.3,
+    offsetZ: 0.2,
+    minDistance: 3,
+    maxDistance: 3,
+    distance: 3,
+  })
+
+  private assetLoader = inject(AssetLoader)
+
+  async onInit(): Promise<void> {
+    const camera = this.thirdPartyCamera.camera.instance
+    camera.fov = 30
+    camera.updateProjectionMatrix()
+
+    const { scene, animations } = await this.assetLoader.geGltfAtPath('dungeon/character-human.glb')
+
+    this.object.add(scene)
+
+    const clips = Object.fromEntries(animations.map(clip => [clip.name, clip]))
+  
+    const idle = new AnimationState(clips.idle)
+    const walk = new AnimationState(clips.walk)
+    const sit = new AnimationState(clips.sit)
+    const jump = new AnimationState(clips.jump)
+    const sprint = new AnimationState(clips.sprint)
+
+    idle.transitionsBetween(walk, () => this.movement.horizontalSpeed > 0)
+    walk.transitionsBetween(sprint, () => this.movement.isSprinting)
+    idle.transitionsTo(sit, elapsedTime => elapsedTime > 1)
+    sit.transitionsTo(walk, () => this.movement.horizontalSpeed > 0)
+  
+    for (const state of [idle, walk, sit, sprint]) {
+      state.transitionsBetween(jump, () => this.movement.mode === CharacterMovementMode.falling)
+    }
+
+    const sm = new AnimationStateMachine(idle)
+
+    this.animation.setup(scene)
+    this.animation.playStateMachine(sm)
+
+
+  }
+
+}
+
+export default Character
---
+++ src/services/player-controller.ts
@@ -0,0 +1,83 @@
+import { inject, Service } from "@hology/core/gameplay"
+import {
+  InputService,
+  Keybind,
+  Mousebind,
+  Wheelbind,
+} from "@hology/core/gameplay/input"
+import Character from "../actors/character"
+
+enum InputAction {
+  moveForward,
+  moveBackward,
+  moveLeft,
+  moveRight,
+  jump,
+  sprint,
+  crouch,
+  rotate,
+  rotateCamera,
+  zoomCamera
+}
+
+@Service()
+class PlayerController {
+  private inputService = inject(InputService) // maybe support multple input services
+  private character: Character
+
+  constructor() {
+    // TODO Add a mouse click feature
+    this.inputService.setKeybind(InputAction.jump, new Keybind(" "))
+    this.inputService.setKeybind(InputAction.sprint, new Keybind("Shift"))
+    this.inputService.setKeybind(InputAction.moveForward, new Keybind("w"))
+    this.inputService.setKeybind(InputAction.moveBackward, new Keybind("s"))
+    this.inputService.setKeybind(InputAction.moveLeft, new Keybind("a"))
+    this.inputService.setKeybind(InputAction.moveRight, new Keybind("d"))
+    this.inputService.setMousebind(
+      InputAction.rotate,
+      new Mousebind(0.01, true, "x")
+    )
+    this.inputService.setMousebind(
+      InputAction.rotateCamera,
+      new Mousebind(0.003, false, "y")
+    )
+    this.inputService.setWheelbind(
+      InputAction.zoomCamera,
+      new Wheelbind(0.0003, false)
+    )
+  }
+
+  public posess(character: Character) {
+    this.inputService.stop()
+    this.character = character
+    this.bindCharacterInput()
+  }
+
+  private bindCharacterInput() {
+    const playerMove = this.character.movement.directionInput
+    const playerJump = this.character.movement.jumpInput
+    const playerSprint = this.character.movement.sprintInput
+
+    this.inputService.bindToggle(InputAction.jump, playerJump.toggle)
+    this.inputService.bindToggle(InputAction.sprint, playerSprint.toggle)
+    this.inputService.bindToggle(InputAction.moveForward, playerMove.togglePositiveY)
+    this.inputService.bindToggle(InputAction.moveBackward, playerMove.toggleNegativeY)
+    this.inputService.bindToggle(InputAction.moveLeft, playerMove.toggleNegativeX)
+    this.inputService.bindToggle(InputAction.moveRight, playerMove.togglePositiveX)
+    this.inputService.bindDelta(
+      InputAction.rotate,
+      this.character.movement.rotationInput.rotateY
+    )
+    this.inputService.bindDelta(
+      InputAction.rotateCamera,
+      this.character.thirdPartyCamera.rotationInput.rotateX
+    )
+    this.inputService.bindDelta(
+      InputAction.zoomCamera,
+      this.character.thirdPartyCamera.zoomInput.increment
+    )
+
+  }
+}
+
+export default PlayerController
